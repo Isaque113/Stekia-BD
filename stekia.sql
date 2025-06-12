136e3fa52c564b169e5844fac28cd2b78a958d18
Isaque_XP <123276457+Isaque113@users.noreply.github.com>
2025-06-12 09:37:30 -0300

    Add files via upload
    tabelas prontas

--- stekia.sql
+++ stekia.sql
@@ -3,74 +3,98 @@ USE stekia;
 
 CREATE TABLE usuario_login (
     id_login BIGINT PRIMARY KEY AUTO_INCREMENT,
-    email_login VARCHAR,
-    senha_login VARCHAR
+    email_login VARCHAR(320) NOT NULL,
+    senha_login VARCHAR(60) NOT NULL
 );
 
 CREATE TABLE checkpoint_mapa (
-    id_checpoint INT
+    id_checpoint INT(4) PRIMARY KEY AUTO_INCREMENT,
+    nome_checkpoint VARCHAR(20),
+    descricao_checpoint VARCHAR(100)
 );
 
 CREATE TABLE jogador (
     id_jogador BIGINT PRIMARY KEY AUTO_INCREMENT,
-    id_login_FK BIGINT,
-    checkpoint_jagador_FK INT,
-    nome_jogador VARCHAR,
-    status_conta_jogador VARCHAR,
-    ultimo_login_jogador DATETIME,
-    FOREIGN KEY (checkpoint_jagador_FK) REFERENCES checpoint_mapa(id_checpoint),
-    FOREIGN KEY (id_login_FK) REFERENCES usuario_login(id_login)
+    id_loginFK BIGINT NOT NULL,
+    checkpoint_jagadorFK INT(4) NOT NULL,
+    nome_jogador VARCHAR(20) NOT NULL,
+    status_conta_jogador VARCHAR(10),
+    ultimo_login_jogador SMALLDATETIME,
+    FOREIGN KEY (checkpoint_jagadorFK) REFERENCES checpoint_mapa(id_checpoint),
+    FOREIGN KEY (id_loginFK) REFERENCES usuario_login(id_login)
+);
+
+--max de 99999 de itens diferentes
+CREATE TABLE item (
+    id_item INT (5) PRIMARY KEY AUTO_INCREMENT,
+    descricao_item VARCHAR(100)
+)
+
+-- max dde 999 racas
+CREATE TABLE raca (
+    id_raca INT(3) PRIMARY KEY AUTO_INCREMENT,
+    nome_raca VARCHAR(20),
+    descricao_raca VARCHAR(100)
+);
+
+--max de 999 classes
+CREATE TABLE classe (
+    id_classe INT(3) PRIMARY KEY AUTO_INCREMENT,
+    nome_classe VARCHAR(20),
+    descricao_classe VARCHAR(100)
+);
+
+--max de 999 habilidades
+CREATE TABLE habilidade (
+    id_habilidade INT(3) PRIMARY KEY AUTO_INCREMENT,
+    nome_habilidade VARCHAR(30),
+    descricao_classe VARCHAR(100)
 );
 
 CREATE TABLE atributo_jogador (
     id_atributo_jogador BIGINT PRIMARY KEY AUTO_INCREMENT,
-    id_jogador_FK BIGINT,
-    id_raca_FK INT,
-    id_classe_FK INT,
-    habilidade_jogador VARCHAR,
-    hp_jogador INT,
-    xp_jogador BIGINT,
-    forca_jogador INT,
-    foco_jogador INT,
-    reflexo_jogador INT,
-    mente_jogador INT,
-    FOREIGN KEY (id_jogador_FK) REFERENCES jogador(id_jogador),
-    FOREIGN KEY (id_raca_FK) REFERENCES raca(id_raca),
-    FOREIGN KEY (id_classe_FK) REFERENCES classe(id_classe)
+    id_jogadorFK BIGINT,
+    id_racaFK INT(3),
+    id_classeFK INT(3),
+    id_habilidadeFK VARCHAR(3),
+    hp_jogador INT(7),
+    xp_jogador INT,
+    forca_jogador INT(3),
+    foco_jogador INT(3),
+    reflexo_jogador INT(3),
+    mente_jogador INT(3),
+    FOREIGN KEY (id_jogadorFK) REFERENCES jogador(id_jogador),
+    FOREIGN KEY (id_racaFK) REFERENCES raca(id_raca),
+    FOREIGN KEY (id_classeFK) REFERENCES classe(id_classe),
+    FOREIGN KEY (id_habilidadeFK) REFERENCES habilidade(id_habilidade)
 );
 
-CREATE TABLE raca (
-    id_raca INT PRIMARY KEY AUTO_INCREMENT,
-);
-
-CREATE TABLE classe (
-    id_classe BIGINT PRIMARY KEY AUTO_INCREMENT,
-);
-
+--max de 999 slots de inventario
 CREATE TABLE inventario (
     id_inventario BIGINT PRIMARY KEY AUTO_INCREMENT,
-    id_slot_inventario INT PRIMARY KEY AUTO_INCREMENT,
-    id_jogador_FK BIGINT,
-    id_item_FK INT,
-    quantidade_total_do_slot INT
-    FOREIGN KEY (id_jogador_FK) REFERENCES jogador(id_jogador),
-    FOREIGN KEY (id_item_FK) REFERENCES item(id_item)
+    id_jogadorFK BIGINT NOT NULL,
+    id_itemFK INT(5),
+    slot_inventario INT(3),
+    FOREIGN KEY (id_jogadorFK) REFERENCES jogador(id_jogador),
+    FOREIGN KEY (id_itemFK) REFERENCES item(id_item)
 );
 
+--max de 9999 NPCs
 CREATE TABLE npc (
-    id_npc INT PRIMARY KEY AUTO_INCREMENT
+    id_npc INT(4) PRIMARY KEY AUTO_INCREMENT,
+    nome_npc VARCHAR(20),
+    descricao_npc(100)
 );
 
+--max de 99999 pontos de amizade
+--modelo da PK, ex: "1122334455667788990-1111" feito com o id do jogador e do npc
 CREATE TABLE amizade_jogador (
-    id_jogador_FK BIGINT,
-    id_npc_FK INT,
-    pontos_amizade INT
+    id_amizade_NPC VARCHAR(24) 
+    id_jogadorFK BIGINT,
+    id_npcFK INT(4),
+    pontos_amizade INT(5)
 );
 
-CREATE TABLE item (
-    id_item INT PRIMARY KEY AUTO_INCREMENT,
-    descricao_item VARCHAR
-)
 
 /*
 https://docs.google.com/document/d/1E0A-FJHErcz_sHSzqO6JAmdunl-YpohUC_fhxepxqM0/edit?tab=t.0
